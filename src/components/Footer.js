0dae2b62f5b02e6b36d97e580c0fb1583b2159d4
legolas2k16 <legolas2k16@gmail.com>
2022-02-21 00:34:35 +0530

    link changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
       <div className="flex justify-center mt-4">
         <a
           className="text-xl m-1 p-1 sm:m-2  sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white transition-colors duration-300"
-          href="https://github.com/backslashflutter"
+          href="https://github.com/dante-san"
         >
           <FaGithub />
           <span class="sr-only">GitHub</span>
@@ -15,7 +15,7 @@ function Footer() {
 
         <a
           className="text-xl m-1 p-1 sm:m-2  sm:p-2 text-red-600 hover:bg-red-600 rounded-full hover:text-white transition-colors duration-300"
-          href="https://github.com/backslashflutter"
+          href="https://youtube.com/saiyanea"
         >
           <FaYoutube />
           <span class="sr-only">YoutTube</span>
@@ -23,7 +23,7 @@ function Footer() {
 
         <a
           className="text-xl m-1 p-1 sm:m-2  sm:p-2 text-blue-500 hover:bg-blue-500 rounded-full hover:text-white transition-colors duration-300"
-          href="https://github.com/backslashflutter"
+          href="https://twitter.com/sledgemaster2"
         >
           <FaTwitter />
           <span class="sr-only">Twitter</span>
@@ -31,16 +31,26 @@ function Footer() {
 
         <a
           className="text-xl m-1 p-1 sm:m-2  sm:p-2 text-teal-500 hover:bg-teal-500 rounded-full hover:text-white transition-colors duration-300"
-          href="https://github.com/backslashflutter"
+          href="https://github.com/dante-san"
         >
           <FaEnvelope />
           <span class="sr-only">Email</span>
         </a>
       </div>
       <div className="flex justify-center mt-4">
-          <p className="text-black mb-4">
-              Made with <span className="mr-2" role="link" aria-label="heart">💝</span> by  <a className="text-blue-500 hover:underline" href="mailto:papu.team7@gmail.com">Laxmidhar Moharana</a>
-          </p>
+        <p className="text-black mb-4">
+          Made with{" "}
+          <span className="mr-2" role="link" aria-label="heart">
+            💝
+          </span>{" "}
+          by{" "}
+          <a
+            className="text-blue-500 hover:underline"
+            href="mailto:papu.team7@gmail.com"
+          >
+            Laxmidhar Moharana
+          </a>
+        </p>
       </div>
     </div>
   );
